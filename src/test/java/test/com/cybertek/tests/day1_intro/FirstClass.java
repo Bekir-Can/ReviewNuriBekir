c83c3930b48ededac663ae57167561b7cdb111a8
Bekir-Can <ruikosta03@gmail.com>
2021-04-25 02:05:27 +0300

    first commit

---
+++ src/test/java/test/com/cybertek/tests/day1_intro/FirstClass.java
@@ -0,0 +1,15 @@
+package test.com.cybertek.tests.day1_intro;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstClass {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://cybertekschool.com");
+        Thread.sleep(3);
+        driver.close();
+    }
+}
